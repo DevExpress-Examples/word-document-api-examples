4f71a944929e45afbdeccf0c877b95cf3508f2e7
geumjaLee <polina.fedorova@devexpress.com>
2020-06-15 11:36:44 +0300

    add custom xml parts examples

---
+++ CS/CodeExamples/CustomXmlActions.cs
@@ -0,0 +1,95 @@
+﻿using DevExpress.XtraRichEdit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace RichEditDocumentServerAPIExample.CodeExamples
+{
+    class CustomXmlActions
+    {
+        static void AddCustomXmlPart(RichEditDocumentServer wordProcessor)
+        {
+            #region #AddCustomXmlPart
+            Document document = wordProcessor.Document;
+            document.AppendText("This document contains custom XML parts.");
+            // Add an empty custom XML part.
+            ICustomXmlPart xmlItem = document.CustomXmlParts.Add();
+            // Populate the XML part with content.
+            XmlElement elem = xmlItem.CustomXmlPartDocument.CreateElement("Employees");
+            elem.InnerText = "Stephen Edwards";
+            xmlItem.CustomXmlPartDocument.AppendChild(elem);
+
+            // Use a string to specify the content for a custom XML part.
+            string xmlString = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <Employees>
+                                <FirstName>Stephen</FirstName>
+                                <LastName>Edwards</LastName>
+                                <Address>4726 - 11th Ave. N.E.</Address>
+                                <City>Seattle</City>
+                                <Region>WA</Region>
+                                <PostalCode>98122</PostalCode>
+                                <Country>USA</Country>
+                            </Employees>";
+            document.CustomXmlParts.Insert(1, xmlString);
+
+            // Add a custom XML part from a file.
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Documents\\Employees.xml");
+            document.CustomXmlParts.Add(xmlDoc);
+            #endregion #AddCustomXmlPart
+        }
+
+        static void AccessCustomXmlPart(RichEditDocumentServer wordProcessor)
+        {
+            #region #AccessCustomXmlPart
+            Document document = wordProcessor.Document;
+            // Load a document.
+            document.LoadDocument("Documents\\CustomXmlParts.docx");
+            // Access a custom XML file stored in the document.
+            XmlDocument xmlDoc = document.CustomXmlParts[0].CustomXmlPartDocument;
+            // Retrieve employee names from the XML file and display them in the document.
+            XmlNodeList nameList = xmlDoc.GetElementsByTagName("Name");
+            document.AppendText("Employee list:");
+            foreach (XmlNode name in nameList)
+            {
+                document.AppendText("\r\n \u00B7 " + name.InnerText);
+            }
+            #endregion #AccessCustomXmlPart
+        }
+
+        static void RemoveCustomXmlPart(RichEditDocumentServer wordProcessor)
+        {
+            #region #RemoveCustomXmlPart
+            Document document = wordProcessor.Document;
+            document.AppendText("This document contains custom XML parts.");
+
+            // Add the first custom XML part.
+            string xmlString1 = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <Employees>
+                                <FirstName>Stephen</FirstName>
+                                <LastName>Edwards</LastName>
+                            </Employees>";
+            var xmlItem1 = document.CustomXmlParts.Add(xmlString1);
+
+            // Add the second custom XML part.
+            string xmlString2 = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <Employees>
+                                <FirstName>Andrew</FirstName>
+                                <LastName>Fuller</LastName>
+                            </Employees>";
+            var xmlItem2 = document.CustomXmlParts.Add(xmlString2);
+
+            // Remove the first item from the collection.
+            document.CustomXmlParts.Remove(xmlItem1);
+            // Use the RemoveAt method to remove an item at the specified position from the collection.
+            // document.CustomXmlParts.RemoveAt(0);
+            // Use the Clear method to remove all items from the collection.
+            // document.CustomXmlParts.Clear();
+            #endregion #RemoveCustomXmlPart
+        }
+    }
+}
---
+++ VB/CodeExamples/CustomXmlActions.vb
@@ -0,0 +1,89 @@
+﻿Imports DevExpress.XtraRichEdit
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Xml
+Imports DevExpress.XtraRichEdit.API.Native
+
+Namespace RichEditDocumentServerAPIExample.CodeExamples
+	Friend Class CustomXmlActions
+		Private Shared Sub AddCustomXmlPart(ByVal wordProcessor As RichEditDocumentServer)
+'			#Region "#AddCustomXmlPart"
+			Dim document As Document = wordProcessor.Document
+			document.AppendText("This document contains custom XML parts.")
+			' Add an empty custom XML part.
+			Dim xmlItem As ICustomXmlPart = document.CustomXmlParts.Add()
+			' Populate the XML part with content.
+			Dim elem As XmlElement = xmlItem.CustomXmlPartDocument.CreateElement("Employees")
+			elem.InnerText = "Stephen Edwards"
+			xmlItem.CustomXmlPartDocument.AppendChild(elem)
+
+			' Use a string to specify the content for a custom XML part.
+			Dim xmlString As String = "<?xml version=""1.0"" encoding=""UTF-8""?>" & ControlChars.CrLf & _
+"                            <Employees>" & ControlChars.CrLf & _
+"                                <FirstName>Stephen</FirstName>" & ControlChars.CrLf & _
+"                                <LastName>Edwards</LastName>" & ControlChars.CrLf & _
+"                                <Address>4726 - 11th Ave. N.E.</Address>" & ControlChars.CrLf & _
+"                                <City>Seattle</City>" & ControlChars.CrLf & _
+"                                <Region>WA</Region>" & ControlChars.CrLf & _
+"                                <PostalCode>98122</PostalCode>" & ControlChars.CrLf & _
+"                                <Country>USA</Country>" & ControlChars.CrLf & _
+"                            </Employees>"
+			document.CustomXmlParts.Insert(1, xmlString)
+
+			' Add a custom XML part from a file.
+			Dim xmlDoc As New XmlDocument()
+			xmlDoc.Load("Documents\Employees.xml")
+			document.CustomXmlParts.Add(xmlDoc)
+'			#End Region ' #AddCustomXmlPart
+		End Sub
+
+		Private Shared Sub AccessCustomXmlPart(ByVal wordProcessor As RichEditDocumentServer)
+'			#Region "#AccessCustomXmlPart"
+			Dim document As Document = wordProcessor.Document
+			' Load a document.
+			document.LoadDocument("Documents\CustomXmlParts.docx")
+			' Access a custom XML file stored in the document.
+			Dim xmlDoc As XmlDocument = document.CustomXmlParts(0).CustomXmlPartDocument
+			' Retrieve employee names from the XML file and display them in the document.
+			Dim nameList As XmlNodeList = xmlDoc.GetElementsByTagName("Name")
+			document.AppendText("Employee list:")
+			For Each name As XmlNode In nameList
+				document.AppendText(ControlChars.CrLf & " " & ChrW(&H00B7).ToString() & " " & name.InnerText)
+			Next name
+'			#End Region ' #AccessCustomXmlPart
+		End Sub
+
+		Private Shared Sub RemoveCustomXmlPart(ByVal wordProcessor As RichEditDocumentServer)
+'			#Region "#RemoveCustomXmlPart"
+			Dim document As Document = wordProcessor.Document
+			document.AppendText("This document contains custom XML parts.")
+
+			' Add the first custom XML part.
+			Dim xmlString1 As String = "<?xml version=""1.0"" encoding=""UTF-8""?>" & ControlChars.CrLf & _
+"                            <Employees>" & ControlChars.CrLf & _
+"                                <FirstName>Stephen</FirstName>" & ControlChars.CrLf & _
+"                                <LastName>Edwards</LastName>" & ControlChars.CrLf & _
+"                            </Employees>"
+			Dim xmlItem1 = document.CustomXmlParts.Add(xmlString1)
+
+			' Add the second custom XML part.
+			Dim xmlString2 As String = "<?xml version=""1.0"" encoding=""UTF-8""?>" & ControlChars.CrLf & _
+"                            <Employees>" & ControlChars.CrLf & _
+"                                <FirstName>Andrew</FirstName>" & ControlChars.CrLf & _
+"                                <LastName>Fuller</LastName>" & ControlChars.CrLf & _
+"                            </Employees>"
+			Dim xmlItem2 = document.CustomXmlParts.Add(xmlString2)
+
+			' Remove the first item from the collection.
+			document.CustomXmlParts.Remove(xmlItem1)
+			' Use the RemoveAt method to remove an item at the specified position from the collection.
+			' document.CustomXmlParts.RemoveAt(0);
+			' Use the Clear method to remove all items from the collection.
+			' document.CustomXmlParts.Clear();
+'			#End Region ' #RemoveCustomXmlPart
+		End Sub
+	End Class
+End Namespace
