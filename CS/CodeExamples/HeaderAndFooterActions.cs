20d387ab6c92ff0784dcb2ac3734387c438e90de
geumjaLee <polina.fedorova@devexpress.com>
2021-04-06 20:28:39 +0300

    update header example

--- CS/CodeExamples/HeaderAndFooterActions.cs
+++ CS/CodeExamples/HeaderAndFooterActions.cs
@@ -11,16 +11,13 @@ namespace RichEditEDocumentServerExample.CodeExamples
             #region #CreateHeader
             Document document = wordProcessor.Document;
             Section firstSection = document.Sections[0];
-            // Create an empty header.
-            SubDocument newHeader = firstSection.BeginUpdateHeader();
-            firstSection.EndUpdateHeader(newHeader);
+
             // Check whether the document already has a header (the same header for all pages).
-            if (firstSection.HasHeader(HeaderFooterType.Primary))
+            if (!firstSection.HasHeader(HeaderFooterType.Primary))
             {
-                SubDocument headerDocument = firstSection.BeginUpdateHeader();
-                document.ChangeActiveDocument(headerDocument);
-                document.CaretPosition = headerDocument.CreatePosition(0);
-                firstSection.EndUpdateHeader(headerDocument);
+                SubDocument newHeader = firstSection.BeginUpdateHeader();
+                newHeader.AppendText("Header");
+                firstSection.EndUpdateHeader(newHeader);
             }
             #endregion #CreateHeader
         }
